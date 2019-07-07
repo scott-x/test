409cc8532f499aeeff286145ad51d45da67d3314
scottxiong <github.scott.bear@gmail.com>
2019-07-08 01:08:12 +0800

    test go package

---
+++ test.go
@@ -0,0 +1,15 @@
+/*
+* @Author: scottxiong
+* @Date:   2019-07-08 01:06:23
+* @Last Modified by:   scottxiong
+* @Last Modified time: 2019-07-08 01:07:26
+ */
+package test
+
+import (
+	"stings"
+)
+
+func Upper(s string) string {
+	return strings.ToUpper(s)
+}
